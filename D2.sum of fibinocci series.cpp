22a683f34bac2c507602057a178c2ab212d885db
Tejusvardhan-74 <112751882+Tejusvardhan-74@users.noreply.github.com>
2022-10-06 10:47:20 +0530

    Add files via upload

---
+++ D2.sum of fibinocci series.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+ 
+// Returns sum of even Fibonacci numbers which are
+// less than or equal to given limit.
+int evenFibSum(int limit)
+{
+    if (limit < 2)
+        return 0;
+ 
+    // Initialize first two even  Fibonacci  numbers
+    // and their sum
+    long long int ef1 = 0, ef2 = 2;
+    long long int sum = ef1 + ef2;
+ 
+    // calculating sum of even Fibonacci value
+    while (ef2 <= limit)
+    {
+        // get next even value of Fibonacci sequence
+        long long int ef3 = 4*ef2 + ef1;
+ 
+        // If we go beyond limit, we break loop
+        if (ef3 > limit)
+            break;
+ 
+        // Move to next even number and update sum
+        ef1 = ef2;
+        ef2 = ef3;
+        sum += ef2;
+    }
+ 
+    return sum;
+}
